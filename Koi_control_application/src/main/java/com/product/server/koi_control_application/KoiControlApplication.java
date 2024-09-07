1581f295612ef5349e656534a8c40dcf2d5339a4
Võ Hoàng Trung <55863018+trungksdoa@users.noreply.github.com>
2024-09-07 19:41:08 +0700

    Move 2 files to root repo

---
+++ Koi_control_application/src/main/java/com/product/server/koi_control_application/KoiControlApplication.java
@@ -0,0 +1,13 @@
+package com.product.server.koi_control_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoiControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoiControlApplication.class, args);
+    }
+
+}
---
+++ Koi_control_application/src/test/java/com/product/server/koi_control_application/KoiControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.server.koi_control_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoiControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
