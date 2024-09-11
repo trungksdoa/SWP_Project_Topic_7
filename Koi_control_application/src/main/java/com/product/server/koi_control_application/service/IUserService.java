f962389cee70f6cfe4e83afca3bd64c661b83cc8
Võ Hoàng Trung <55863018+trungksdoa@users.noreply.github.com>
2024-09-11 22:18:26 +0700

    Updated setup host project to cloud

--- Koi_control_application/src/main/java/com/product/server/koi_control_application/service/IUserService.java
+++ Koi_control_application/src/main/java/com/product/server/koi_control_application/service/IUserService.java
@@ -3,7 +3,6 @@ package com.product.server.koi_control_application.service;
 import com.product.server.koi_control_application.model.Users;
 import org.springframework.data.domain.Page;
 
-import java.util.List;
 
 public interface IUserService {
     void saveUser(Users user);
