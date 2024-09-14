709c8847104ec57cc5025f7bb859a72671a8661a
Võ Hoàng Trung <55863018+trungksdoa@users.noreply.github.com>
2024-09-14 09:15:36 +0700

    Changes

---
+++ Koi_control_application/src/main/java/com/product/server/koi_control_application/config/EmailSender.java
@@ -0,0 +1,29 @@
+package com.product.server.koi_control_application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailSender {
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("trungvhse182490@fpt.edu.vn");
+        mailSender.setPassword("0335857134");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
