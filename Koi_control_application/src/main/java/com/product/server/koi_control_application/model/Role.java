e0baa05bc792e73d1c370d9e8cb4dafe1d19c7c8
Võ Hoàng Trung <55863018+trungksdoa@users.noreply.github.com>
2024-09-15 21:36:02 +0700

    Remove unused code and update service and controller for user, order, product, and cart

--- Koi_control_application/src/main/java/com/product/server/koi_control_application/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.product.server.koi_control_application.model;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- Koi_control_application/src/main/java/com/product/server/koi_control_application/repository/ProductImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.product.server.koi_control_application.repository;
-
-import com.product.server.koi_control_application.model.ProductImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductImageRepository extends JpaRepository<ProductImage, Integer> {
-}
