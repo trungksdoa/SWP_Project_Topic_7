c217fffb2d1ee6fde89fb1403d00ffec78ddfe60
Võ Hoàng Trung <55863018+trungksdoa@users.noreply.github.com>
2024-09-17 17:23:35 +0700

    Remove unused code

--- Koi_control_application/src/main/java/com/product/server/koi_control_application/customException/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.product.server.koi_control_application.customException;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super("Product with name: " + message + " not found");
-    }
-}
--- Koi_control_application/src/main/java/com/product/server/koi_control_application/customException/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.product.server.koi_control_application.customException;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String value) {
-        super("User not found with id: " + value);
-    }
-}
