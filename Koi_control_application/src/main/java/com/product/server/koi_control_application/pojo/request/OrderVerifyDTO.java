0366aa645b0cb3dc85376567b7a88738af73b288
Võ Hoàng Trung <55863018+trungksdoa@users.noreply.github.com>
2024-10-06 11:13:59 +0700

    Update order API CONFIRM ORDER AND RECEIVE ORDER

---
+++ Koi_control_application/src/main/java/com/product/server/koi_control_application/pojo/request/OrderVerifyDTO.java
@@ -0,0 +1,13 @@
+package com.product.server.koi_control_application.pojo.request;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderVerifyDTO {
+    private int orderId;
+}
