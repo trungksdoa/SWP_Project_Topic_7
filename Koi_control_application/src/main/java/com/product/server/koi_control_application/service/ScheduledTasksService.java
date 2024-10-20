d651095cb75d870f43c91d330722ecdf8d4e03dd
Võ Hoàng Trung <55863018+trungksdoa@users.noreply.github.com>
2024-10-21 01:38:59 +0700

    Update report api

--- Koi_control_application/src/main/java/com/product/server/koi_control_application/service/ScheduledTasksService.java
+++ Koi_control_application/src/main/java/com/product/server/koi_control_application/service/ScheduledTasksService.java
@@ -8,6 +8,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -15,16 +17,16 @@ public class ScheduledTasksService {
 
     private final OrderRepository orderRepository;
 
-//    @Scheduled(cron = "9 * * * * *")
-//    @Transactional
-//    public void simulatorDeliveredOrders() {
-//        int updatedCount = orderRepository.updateSimulatorOrder(
-//                OrderCode.DELIVERED.getValue(),
-//                OrderCode.SHIPPING.getValue()
-//        );
-//        log.info("Simulator  {} orders from SHIPPING to DELIVERED status", updatedCount);
-//    }
-//
+    @Scheduled(cron = "9 * * * * *")
+    @Transactional
+    public void simulatorDeliveredOrders() {
+        int updatedCount = orderRepository.updateSimulatorOrder(
+                OrderCode.DELIVERED.getValue(),
+                OrderCode.SHIPPING.getValue()
+        );
+        log.info("Simulator  {} orders from SHIPPING to DELIVERED status", updatedCount);
+    }
+
 //    @Scheduled(cron = "${app.schedule.task.update-order-status.cron}") // Runs at 1:00 AM every day
 //    @Transactional
 //    public void updatePendingOrders() {
