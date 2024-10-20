9ad7cf927b172befe0df1866aaeab1cce499c51d
Võ Hoàng Trung <55863018+trungksdoa@users.noreply.github.com>
2024-10-20 23:00:38 +0700

    Update report api

--- Koi_control_application/src/main/java/com/product/server/koi_control_application/controller/AIReport.java
+++ Koi_control_application/src/main/java/com/product/server/koi_control_application/controller/AIReport.java
@@ -1,24 +1,15 @@
 package com.product.server.koi_control_application.controller;
 
-import com.product.server.koi_control_application.model.KoiFish;
-import com.product.server.koi_control_application.pojo.response.BaseResponse;
-import com.product.server.koi_control_application.service.MLService;
 import com.product.server.koi_control_application.serviceInterface.IKoiFishService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/ai")
 @RequiredArgsConstructor
 public class AIReport {
     private final IKoiFishService iKoiFishService;
-    private final MLService mlService;
+//    private final MLService mlService;
 
 //
 //    @GetMapping("/{koiFishId}/predict-growth-and-feeding")
